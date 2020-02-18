52cbdc6d47c7a4709ae1ddbb7714bec8983f33aa
clay3899 <chv11@georgetown.edu>
2020-02-17 21:39:55 -0500

    last refreshed date
    Added the last refreshed date

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -14,10 +14,13 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+#breakpoint()
 
 
-quit()
+#quit()
 
 symbol = "0"
 symbol_list =[]
@@ -46,7 +49,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print("LATEST DAY: ", last_refreshed)
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
